023c52ce971efb46977a706335205a9cdb68e427
mark <markclarus1990@gmail.com>
2026-07-05 14:24:56 +0800

    feat: use local product images instead of generated SVGs

---
+++ supabase/migrations/20260706000000_update_product_images.sql
@@ -0,0 +1,5 @@
+UPDATE products SET image_url = '/products/coke.PNG' WHERE name = 'Coke';
+UPDATE products SET image_url = '/products/pancitcanton.PNG' WHERE name = 'PancitCanton';
+UPDATE products SET image_url = '/products/popcorn.PNG' WHERE name = 'Popcorn';
+UPDATE products SET image_url = '/products/icejuice.PNG' WHERE name = 'IceJuice';
+UPDATE products SET image_url = '/products/palamig.PNG' WHERE name = 'Palamig';
